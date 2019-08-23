ad277e21930530c3a65187ce2e0c77c03c26fda1
papageg <greggordon98@gmail.com>
2019-08-23 14:59:57 -0700

    save
    save

---
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -0,0 +1,32 @@
+// Function contructor
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher'); // Instantiation
+var jane = new Person('Jane', 1969, 'disigner');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
