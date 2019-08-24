08ab4495b526d3aedf03a46dd97f4224c7f54995
Greg <greggordon98@gmail.com>
2019-08-23 18:15:15 -0700

    saved again
    saved again

--- complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -1,32 +1,48 @@
-// Function contructor
+// // Function contructor
 
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1990,
+//     job: 'teacher'
+// };
+
+// var Person = function(name, yearOfBirth, job){
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person.prototype.calculateAge = function() {
+//     console.log(2016 - this.yearOfBirth);
+// }
+
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('John', 1990, 'teacher'); // Instantiation
+// var jane = new Person('Jane', 1969, 'disigner');
+// var mark = new Person('Mark', 1948, 'retired');
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(mark.lastName);
+
+//Object.create
+var personProto = {
+    calculateAge: function(){
+        console.log(2016 - this.yearOfBirth);
+    }
 };
 
-var Person = function(name, yearOfBirth, job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-}
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-Person.prototype.calculateAge = function() {
-    console.log(2016 - this.yearOfBirth);
-}
-
-Person.prototype.lastName = 'Smith';
-
-var john = new Person('John', 1990, 'teacher'); // Instantiation
-var jane = new Person('Jane', 1969, 'disigner');
-var mark = new Person('Mark', 1948, 'retired');
-
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
-
+var jane = Object.create(personProto, {
+    name: { value: 'Jane'},
+    
+})
