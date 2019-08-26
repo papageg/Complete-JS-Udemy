7f54ea507bb885dab885595ea4107791e575c5d3
papageg <greggordon98@gmail.com>
2019-08-26 14:22:42 -0700

    broke
    broke

--- complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -1,4 +1,4 @@
-// // Function contructor
+// // Function constructor
 
 // var john = {
 //     name: 'John',
@@ -182,21 +182,21 @@
 ///////////////////////////////////////////////////
 //Lecture: Closures
 
-function retirement(retirementAge){
-    var a = ' years left until retirement.';
-    return function(yearOfBirth){
-        var age = 2016 - yearOfBirth;
-        console.log((retirementAge - age) + a);
-    }
-}
+// function retirement(retirementAge){
+//     var a = ' years left until retirement.';
+//     return function(yearOfBirth){
+//         var age = 2016 - yearOfBirth;
+//         console.log((retirementAge - age) + a);
+//     }
+// }
 
-var retirementUS = retirement(66);
-var retirementGermany = retirement(65);
-var retirementIceland = retirement(67);
+// var retirementUS = retirement(66);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
 
-retirementUS(1990);
-retirementGermany(1990);
-retirementIceland(1990);
+// retirementUS(1990);
+// retirementGermany(1990);
+// retirementIceland(1990);
 
 
 
@@ -219,19 +219,149 @@ retirementIceland(1990);
 //     }
 // }
 
-function interviewQuestion(job){
-    return function(name){
-        if(job === 'designer'){
-            console.log(name + ', can you please explain what UX design is?');
-        } else if(job === 'teacher'){
-            console.log('What subject do you teach, ' + name + '?');
-        } else {
-            console.log('Hello ' + name + ' what do you do?');
-        }
-    }
+// function interviewQuestion(job){
+//     return function(name){
+//         if(job === 'designer'){
+//             console.log(name + ', can you please explain what UX design is?');
+//         } else if(job === 'teacher'){
+//             console.log('What subject do you teach, ' + name + '?');
+//         } else {
+//             console.log('Hello ' + name + ' what do you do?');
+//         }
+//     }
+// }
+
+// interviewQuestion('teacher')('John');
+
+
+//////////////////////////////////////////////////////////////
+// Lecture: Bind, Call and Apply
+
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher',
+//     presentation: function(style,timeOfDay){
+//         if(style === 'formal'){
+//             console.log('Good ' + timeOfDay + ' Ladies and gentlemen! I\'m ' + this.name + ' I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//         } else if (style === 'friendly'){
+//             console.log('Hey! What\'s up? I\'m ' + this.name + ' I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//         }
+//     }
+// };
+
+// var emily={
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// }
+
+// john.presentation('formal', 'morning');
+
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+// john.presentation.apply(emily, ['friendly', 'afternoon'])
+
+// var johnFriendly = john.presentation.bind(john, 'friendly')
+
+// johnFriendly('morning');
+// johnFriendly('night');
+
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+// emilyFormal('afternoon');
+
+
+
+
+
+
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn){
+//     var arrRes = [];
+//     for (i= 0; i < arr.length; i++){
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el){
+//     return 2016 - el;
+// }
+
+// function isFullAge(limit, el){
+//     return el >= limit; 
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// console.log(ages);
+// console.log(fullJapan);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+
+// var ranNum = Math.floor(Math.random() * 4);
+
+// function interviewQuestion(qNumber){
+//         if(qNumber === 1){
+//             resturant = prompt('Who has worked in a resturant?');
+//         } else if(qNumber === 2){
+//             carlsJr = prompt('Who has worked in a Carl\'s Jr.?');
+//         }  else if(qNumber === 3){
+//             winCleaner = prompt('Who is a window cleaner?');
+//         }
+//     };
+
+// interviewQuestion(ranNum);
+
+var ranNumber = Math.ceil(Math.random() * 3);
+
+var Question = function(qstion, answer){
+    this.qstion = qstion;
+    this.answer = answer;
 }
 
-interviewQuestion('teacher')('John');
+Question.prototype.Question = function() {
+    ranNumber;
+}
+
+var resturant = new Question('Who worked in a resturant?', 'greg'); // Instantiation
+var carlsJr = new Question('Who worked at Carl\'s Jr.', 'marco');
+var winCleaner = new Question('Who is a window cleaner?', 'gilbert');
+
+
+var allQuestions = [resturant, carlsJr, winCleaner];
+
+
+console.log(allQuestions[ranNumber - 1]);
 
 
 
@@ -243,3 +373,27 @@ interviewQuestion('teacher')('John');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
