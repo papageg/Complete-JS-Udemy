72e84d54dc024cb9582e977fbc1f4890f8239be6
papageg <greggordon98@gmail.com>
2019-08-28 13:43:16 -0700

    start
    start

---
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -0,0 +1,13 @@
+// UI MODULE : Get input values
+//Add the new item to the UI
+//Update the UI
+
+
+
+// Data Module:
+//Add the new item to our data structure
+//Calculate budget
+
+
+//Controller Module:
+// Add Event Handler
